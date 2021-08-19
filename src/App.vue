d3cec9f7fc77d0a7eaf5f902bc326a5109ce8b45
robin-whg <mail@robin-whg.com>
2021-08-19 08:39:24 +0200

    feat: handle props of viewport component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Henlo</h1>
+  <viewport :hdr="hdr" :models="models" />
 </template>
+
+<script>
+import Viewport from "./components/Viewport.vue";
+
+export default {
+  components: {
+    Viewport,
+  },
+  setup() {
+    const hdr =
+      "https://firebasestorage.googleapis.com/v0/b/gs-waermesysteme.appspot.com/o/hdrs%2Fstudio_hdr_1k.hdr?alt=media&token=d3e142a1-dd28-487a-8a2f-2c888a3205b5";
+    const models = [
+      "https://firebasestorage.googleapis.com/v0/b/gs-waermesysteme.appspot.com/o/models%2Fmonkey.gltf?alt=media&token=f7e080d5-acaf-47c1-9bd4-4e23380cb393",
+    ];
+    return { hdr, models };
+  },
+};
+</script>
---
+++ src/components/Viewport.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-gray-400">s</div>
+</template>
+
+<script>
+import { toRefs } from "vue";
+
+export default {
+  props: {
+    hdr: {
+      type: String,
+      required: true,
+    },
+    models: {
+      type: Array,
+    },
+  },
+  setup(props) {
+    const { hdr, models } = toRefs(props);
+    return {};
+  },
+};
+</script>
