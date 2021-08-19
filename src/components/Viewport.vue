4af616d26dffcb787faadc414e15f2951e584e1f
robin-whg <mail@robin-whg.com>
2021-08-19 13:15:07 +0200

    feat: create scene, load hdr + models and frame Scene

--- src/components/Viewport.vue
+++ src/components/Viewport.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="bg-gray-400">s</div>
+  <canvas v-show="!isLoading" v-bind="$attrs" ref="canvas" />
+  <slot v-if="isLoading"></slot>
 </template>
 
 <script>
-import { toRefs } from "vue";
+import { toRefs, ref, onMounted } from "vue";
+import { useViewport } from "@/use/viewport";
 
 export default {
   props: {
@@ -17,7 +19,25 @@ export default {
   },
   setup(props) {
     const { hdr, models } = toRefs(props);
-    return {};
+    const canvas = ref(null);
+    const isLoading = ref(false);
+
+    onMounted(async () => {
+      const { scene, loadHdr, loadModel, frameObject } = useViewport(
+        canvas.value
+      );
+      isLoading.value = true;
+      await loadHdr(hdr.value);
+      await Promise.all(
+        models.value.map(async (uri) => {
+          await loadModel(uri);
+        })
+      );
+      frameObject(scene);
+      isLoading.value = false;
+    });
+
+    return { canvas, isLoading };
   },
 };
 </script>
