d2e7a81f83ff1186d2196a9856039ab840c3be0f
robin-whg <mail@robin-whg.com>
2021-08-20 08:57:33 +0200

    test: file for testing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <viewport :hdr="hdr" :models="models" />
+  <div class="flex">
+    <viewport class="h-96 w-96" :hdr="hdr" :models="models3">
+      <div class="h-96 w-96 flex items-center justify-center">loading</div>
+    </viewport>
+  </div>
 </template>
 
 <script>
-import Viewport from "./components/Viewport.vue";
+import Viewport from "../dist/three-viewer.es.js";
 
 export default {
   components: {
@@ -13,8 +17,9 @@ export default {
     const hdr =
       "https://firebasestorage.googleapis.com/v0/b/gs-waermesysteme.appspot.com/o/hdrs%2Fstudio_hdr_1k.hdr?alt=media&token=d3e142a1-dd28-487a-8a2f-2c888a3205b5";
     const models = [
-      "https://firebasestorage.googleapis.com/v0/b/gs-waermesysteme.appspot.com/o/models%2Fmonkey.gltf?alt=media&token=f7e080d5-acaf-47c1-9bd4-4e23380cb393",
+      "https://threejsfundamentals.org/threejs/resources/models/cartoon_lowpoly_small_city_free_pack/scene.gltf",
     ];
+
     return { hdr, models };
   },
 };
